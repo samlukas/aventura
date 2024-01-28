6004e046731603c673611a9f0f3656883448a494
samlukas <samuelplukas@gmail.com>
2024-01-28 00:15:06 -0500

    created page generation functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -13,3 +14,19 @@ def book():
     if request.method == 'POST':
         print(request.form["story"])
     return render_template('index.html')
+
+@app.route('/get_story')
+def get_story():
+    story = "what da heck"
+    option1 = "do this ___"
+    option2 = "or do this ___"
+    option3 = "or do this ___?"
+    return jsonify(story=story, option1=option1, option2=option2, option3=option3)
+
+@app.route('/process_choice', methods=['POST'])
+def process_data():
+    data_from_js = request.json  # Assuming the data is sent as JSON
+    # Process the data as needed
+    print(data_from_js["option"])
+    result = {'status': 'success', 'message': 'Data received successfully'}
+    return jsonify(result)
