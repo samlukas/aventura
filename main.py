590240e31dae36a5f80eef2c2ac3c81cc35f3072
Junhee Lee <junheelee@Junhees-MacBook-Pro.local>
2024-01-27 19:18:44 -0500

    minor changes in text variables

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import text_gen
 import image_gen
 
+OPTION_SETUP = "Provide list of 3 possibilities of what can happen next. Each option should \
+    briefly introduce the event from the adventure continued after this. Do not write more than \
+    5 words for each option. Do not write anything else except the story and the list. Do not \
+    ask questions to the user. Do not type commands. Do not ask the user for the next action. \
+    Do not write explanations. The user will choose one option within their next input. You will \
+    continue generating the story with the choice of the user as the main event of the adventure. \
+    The format for the list of options should be formatted like this:\nOptions:\n1.\n2.\n3."
+STORY_SETUP = ". The story should be around 200 words. Do not write anything else except the story. \
+    Do not ask questions to the user. Do not type commands. Do not ask the user for the next action. \
+    Do not write explanations. "
+
 if __name__ == "__main__":
     max_turns = 2
 
@@ -18,7 +29,7 @@ if __name__ == "__main__":
     print("_____\nCOVER_PROMPT: " + cover_image_prompt + "\n_____")
     image_gen.image(cover_image_prompt, 1)
     
-    message = "Provide list of 3 possibilities of what can happen next. Each option should briefly introduce the event from the adventure continued after this. Do not write more than 5 words for each option. Do not write anything else except the story and the list. Do not ask questions to the user. Do not type commands. Do not ask the user for the next action. Do not write explanations. The user will choose one option within their next input. You will continue generating the story with the choice of the user as the main event of the adventure. The format for the list of options should be formatted like this:\nOptions:\n1.\n2.\n3."
+    message = OPTION_SETUP
     options = text_gen.options_gen(chat_history, message) # options for story 2
     print(options)
     
@@ -33,7 +44,7 @@ if __name__ == "__main__":
         print(options)
 
         user_choice = input("Enter 1, 2, or 3: ") # user input
-        message = "Continue writing the story following option " + user_choice + ". The story should be around 200 words. Do not write anything else except the story. Do not ask questions to the user. Do not type commands. Do not ask the user for the next action. Do not write explanations. "
+        message = "Continue writing the story following option " + user_choice + STORY_SETUP
         story = text_gen.story_gen(chat_history, story_lst, message) # story
         print("_____\n" + story + "\n_____")
         image_gen.image(text_gen.dalle_prompt_gen(story), n+3)
--- text_gen.py
+++ text_gen.py
@@ -2,12 +2,53 @@ import re
 import cohere
 co = cohere.Client('OiUmG3lHaetSjXbrdx4Pwmvv0KecV5pGiMLsghFm')
 
+EXAMPLE_PROMPTS = "\nExample: An icy landscape under a starlit sky, where a magnificent frozen \
+    waterfall flows over a cliff. In the center of the scene, a fire burns bright, its flames \
+    seemingly frozen in place, casting a shimmering glow on the surrounding ice and snow.\n\
+    Example: A swirling, multicolored portal emerges from the depths of an ocean of coffee, with \
+    waves of the rich liquid gently rippling outward. The portal engulfs a coffee cup, which \
+    serves as a gateway to a fantastical dimension. The surrounding digital art landscape reflects \
+    the colors of the portal, creating an alluring scene of endless possibilities.\nExample: A \
+    mischievous ferret with a playful grin squeezes itself into a large glass jar, surrounded by \
+    colorful candy. The jar sits on a wooden table in a cozy kitchen, and warm sunlight filters \
+    through a nearby window.\nExample: A fierce garden gnome warrior, clad in armor crafted from \
+    leaves and bark, brandishes a tiny sword and shield. He stands valiantly on a rock amidst a \
+    blooming garden, surrounded by colorful flowers and towering plants. A determined expression is \
+    painted on his face, ready to defend his garden kingdom."
+WARNING = "Do not write explanations. Do not ask questions to the user. Do not type commands. \
+    Do not ask the user for the next action. Do not write anything else except the story."
+SETUP_1 = "You are a creative storyteller who will help me build an iterative story that is \
+    adventurous, vivid, descriptive and intriguing. \The story will be in third-person point of view. \
+    Your task is to generate story. You will first receive a general theme or the subject of the \
+    story from the user. Respond with ‘.’ If you understand."
+SETUP_2 = "\", provide the beginning of the vivid and adventurous story. The story should be around \
+    300 words."
+DALLE_SETUP_1 = "You are an expert prompt engineer who writes short, concise and descriptive short \
+    synthetic captions to create a prompt for text to image ML model, Dall-E. Your task is to write \
+    short synthetic captions for Dall-E to generate an image from a story provided by the user. The \
+    generated image should describe a core scene of the story. The generated image should be creative \
+    and involve storytelling. You will receive a story from the user. Respond with ‘.’ If you understand."
+DALLE_SETUP_2 = "These are some examples of the prompt for Dall-E. You will receive a story from \
+    the user. Respond with ‘.’ If you understand." 
+DALLE_SETUP_3 ="\"\nProvide a prompt with short synthetic captions describing the subject of the \
+    story and the style of the image. Use 20 to 30 words to create the prompt for Dall-E."
+COVER_SETUP_1 = "You are an expert prompt engineer who writes short, concise and descriptive short \
+    synthetic captions to create a prompt for text to image ML model, Dall-E. Your task is to write \
+    short synthetic captions for Dall-E to generate a book cover image from a short scenario or \
+    concept provided by the user. The generated image should depict the user’s description. The \
+    generated image should be creative. You will receive a description from the user. Respond with \
+    ‘.’ If you understand."
+COVER_SETUP_2 = "These are some examples of the prompt for Dall-E. You will receive a description \
+    from the user. Respond with ‘.’ If you understand."
+COVER_SETUP_3 = r"\"\nHere is the description: Provide a prompt with short synthetic captions \
+    describing the subject of the story and the style of the image. Use 20 to 30 words to create \
+    a prompt for Dall-E. The prompt should be in a bracket like this: \{Prompt\}."
 
 def story_beginning(chat_history: list[str], story_lst: list[str], prompt: str) -> str:
-    message = "You are a creative storyteller who will help me build an iterative story that is adventurous, vivid, descriptive and intriguing. The story will be in third-person point of view. Your task is to generate story. You will first receive a general theme or the subject of the story from the user. Respond with ‘.’ If you understand."
+    message = SETUP_1
     answer = chat(chat_history, message)
 
-    message = "Given the theme or subject \"" + prompt + "\", provide the beginning of the vivid and adventurous story. The story should be around 300 words. Do not write explanations. Do not ask questions to the user. Do not type commands. Do not ask the user for the next action. Do not write anything else except the story."
+    message = "Given the theme or subject \"" + prompt + SETUP_2
     story = story_gen(chat_history, story_lst, message)
 
     return story
@@ -50,13 +91,13 @@ def story_gen(chat_history: list[str], story_lst: list[str], message: str) -> st
 def dalle_prompt_gen(story: str) -> str:
     chat_history = []
 
-    message = "You are an expert prompt engineer who writes short, concise and descriptive short synthetic captions to create a prompt for text to image ML model, Dall-E. Your task is to write short synthetic captions for Dall-E to generate an image from a story provided by the user. The generated image should describe a core scene of the story. The generated image should be creative and involve storytelling. You will receive a story from the user. Respond with ‘.’ If you understand."
+    message = DALLE_SETUP_1
     answer = chat(chat_history, message)
 
-    message = "These are some examples of the prompt for Dall-E. You will receive a story from the user. Respond with ‘.’ If you understand.\nExample: An icy landscape under a starlit sky, where a magnificent frozen waterfall flows over a cliff. In the center of the scene, a fire burns bright, its flames seemingly frozen in place, casting a shimmering glow on the surrounding ice and snow.\nExample: A swirling, multicolored portal emerges from the depths of an ocean of coffee, with waves of the rich liquid gently rippling outward. The portal engulfs a coffee cup, which serves as a gateway to a fantastical dimension. The surrounding digital art landscape reflects the colors of the portal, creating an alluring scene of endless possibilities.\nExample: A mischievous ferret with a playful grin squeezes itself into a large glass jar, surrounded by colorful candy. The jar sits on a wooden table in a cozy kitchen, and warm sunlight filters through a nearby window."
+    message = DALLE_SETUP_2 + EXAMPLE_PROMPTS
     answer = chat(chat_history, message)
 
-    message = "Here is the story:\n\"" + story + "\"\nProvide a prompt with short synthetic captions describing the subject of the story and the style of the image. Use 20 to 30 words to create the prompt for Dall-E."
+    message = "Here is the story:\n\"" + story + DALLE_SETUP_3
     answer = chat(chat_history, message)
 
     prompt = answer.strip() + " Digital art."
@@ -66,13 +107,13 @@ def dalle_prompt_gen(story: str) -> str:
 
 def cover_image_prompt_gen(prompt: str) -> str:
     chat_history = []
-    message = "You are an expert prompt engineer who writes short, concise and descriptive short synthetic captions to create a prompt for text to image ML model, Dall-E. Your task is to write short synthetic captions for Dall-E to generate a book cover image from a short scenario or concept provided by the user. The generated image should depict the user’s description. The generated image should be creative. You will receive a description from the user. Respond with ‘.’ If you understand."
+    message = COVER_SETUP_1
     answer = chat(chat_history, message)
 
-    message = "These are some examples of the prompt for Dall-E. You will receive a description from the user. Respond with ‘.’ If you understand.\nExample: An icy landscape under a starlit sky, where a magnificent frozen waterfall flows over a cliff. In the center of the scene, a fire burns bright, its flames seemingly frozen in place, casting a shimmering glow on the surrounding ice and snow.\nExample: A swirling, multicolored portal emerges from the depths of an ocean of coffee, with waves of the rich liquid gently rippling outward. The portal engulfs a coffee cup, which serves as a gateway to a fantastical dimension. The surrounding digital art landscape reflects the colors of the portal, creating an alluring scene of endless possibilities.\nExample: A mischievous ferret with a playful grin squeezes itself into a large glass jar, surrounded by colorful candy. The jar sits on a wooden table in a cozy kitchen, and warm sunlight filters through a nearby window."
+    message = COVER_SETUP_2 + EXAMPLE_PROMPTS    
     answer = chat(chat_history, message)
 
-    message = "Here is the description:\n\"" + prompt + r"\"\nHere is the description: Provide a prompt with short synthetic captions describing the subject of the story and the style of the image. Use 20 to 30 words to create a prompt for Dall-E. The prompt should be in a bracket like this: \{Prompt\}."
+    message = "Here is the description:\n\"" + prompt + COVER_SETUP_3
     answer = chat(chat_history, message)
 
     answer_ = re.findall(r"\{.+\}", answer)
@@ -86,7 +127,8 @@ def cover_image_prompt_gen(prompt: str) -> str:
 
 
 def title_gen(prompt: str) -> str:
-    message = "Create a title of a fictional book with the provided theme or subject: \"" + prompt + r"\". The title should be in a bracket like this: \{Title\}."
+    message = "Create a title of a fictional book with the provided theme or subject: \"" + prompt \
+        + r"\". The title should be in a bracket like this: \{Title\}."
     answer = chat([], message)
     title = re.findall(r"\{.+\}", answer)
     if len(title) == 0:
